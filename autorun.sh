fd7ec866afb917665e6206a1f48bb3ce86159d23
persidious <84891817+persidious@users.noreply.github.com>
2021-05-26 23:50:09 -0700

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -3,7 +3,7 @@ apt-get upgrade -y
 apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
 apt-get install git
 git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
-cd /home/persidious/Desktop/ccminer
+cd ccminer
 chmod +x build.sh
 chmod +x configure.sh
 chmod +x autogen.sh
