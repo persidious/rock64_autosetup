372297f060a0eacbb6865d1420632b82448b8d50
persidious <84891817+persidious@users.noreply.github.com>
2021-06-04 23:33:58 -0700

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -9,3 +9,5 @@ chmod +x build.sh
 chmod +x configure.sh
 chmod +x autogen.sh
 ./build.sh
+cd /home/persidious/ccminer
+./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u zs1p7rh4cen99tu36qmf7ays63sufft5nxg8shsw22lgppnrpjygayx7wvwjm87rrrkcmsrgytyqe2.ROCK006 -p x -t 8
